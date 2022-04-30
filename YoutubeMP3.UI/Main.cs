01dcc9851c031db8d163b19a0bef82f1981c3c7d
Adem KOCAMAZ <kocamazadem@gmail.com>
2022-04-30 04:10:29 +0300

    Added YoutubeService.cs

--- YoutubeMP3.UI/Main.cs
+++ YoutubeMP3.UI/Main.cs
@@ -17,93 +17,24 @@ namespace YoutubeMP3.UI
 {
     public partial class Main : Form
     {
+        private YoutubeService youtubeService;
         public Main()
         {
             InitializeComponent();
+            youtubeService = new YoutubeService(richTextBox1, progressBar1,listBox_links);
         }
-
-        private async void DownloadComponents()
-        {
-            richTextBox1.Text = "Downloading youtube-dl.exe and ffmpeg-release-essentials.7z";
-            LoggingTool.log.Info("Downloading youtube-dl.exe and ffmpeg-release-essentials.7z");
-            await Task.Run(() =>
-            {
-                if (File.Exists("youtube-dl.exe"))
-                {
-                    File.Delete("youtube-dl.exe");
-                }
-                if (File.Exists("ffmpeg.exe"))
-                {
-                    File.Delete("ffmpeg.exe");
-                }
-                if (File.Exists("ffprobe.exe"))
-                {
-                    File.Delete("ffprobe.exe");
-                }
-                if (File.Exists("ffmpeg-release-essentials.7z"))
-                {
-                    File.Delete("ffmpeg-release-essentials.7z");
-                }
-                FileDownloader fd = new FileDownloader(progressBar1);
-                fd.Download("https://youtube-dl.org/downloads/latest/youtube-dl.exe", "youtube-dl.exe");
-                fd.Download("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z", "ffmpeg-release-essentials.7z");
-                //Extract(reply => BeginInvoke((MethodInvoker)delegate { listBox_links.Items.Add(reply); }));
-                
-            });
-
-            await Task.Run(Extract);
-
-            richTextBox1.Text = "Güncelleme başarıyla tamamlandı.";
-            LoggingTool.log.Info("Güncelleme başarıyla tamamlandı.");
-            progressBar1.Value = 100;
-        }
-
-        private async void DownloadMP3()
-        {
-            await Task.Run(() =>
-            {
-                ConsoleApp app = new ConsoleApp("youtube-dl.exe", "--verbose --extract-audio --audio-format mp3 --batch-file url.txt");
-                app.ConsoleOutput += App_ConsoleOutput;
-                app.Run();
-                app.WaitForExit();
-                richTextBox1.Text = "İndirme başarıyla tamamlandı.";
-                LoggingTool.log.Info("İndirme başarıyla tamamlandı.");
-                listBox_links.Items.Clear();
-            });
-            
-        }
-
-        private void App_ConsoleOutput(object sender, ConsoleOutputEventArgs e)
-        {
-            richTextBox1.Text = e.Line;
-            LoggingTool.log.Info(e.Line);
-        }
-
-        private void Extract()
-        {
-            progressBar1.Value = 70;
-            ConsoleApp app = new ConsoleApp("7z.exe", "e -r ffmpeg-release-essentials.7z ffmpeg.exe ffprobe.exe");
-            app.ConsoleOutput += App_ConsoleOutput;
-            app.Run();
-            app.WaitForExit();
-            if (File.Exists("ffmpeg-release-essentials.7z"))
-            {
-                File.Delete("ffmpeg-release-essentials.7z");
-            }
-        }
-        //private void Extract(Action<string> replyHandler)
-        //{
-        //    ConsoleApp app = new ConsoleApp("7z.exe", "e -r ffmpeg-release-essentials.7z ffmpeg.exe ffprobe.exe");
-        //    app.ConsoleOutput += App_ConsoleOutput;    
-        //    app.Run();
-        //    app.WaitForExit();
-        //}
-
 
         private void button_youtubedl_Click(object sender, EventArgs e)
         {
-            progressBar1.Value = 20;
-            DownloadComponents();
+            try
+            {
+                youtubeService.DownloadComponents();
+            }
+            catch (Exception exception)
+            {
+                LoggingTool.log.Error("youtubeService.DownloadComponents()", exception);
+            }
+            
         }
 
         private void button_Add_Click(object sender, EventArgs e)
@@ -120,24 +51,33 @@ namespace YoutubeMP3.UI
 
         private void button_Start_Click(object sender, EventArgs e)
         {
-            if (listBox_links.Items.Count == 0)
+            try
             {
-                return;
-            }
-            StreamWriter saveFile = new StreamWriter("url.txt");
-            foreach (var item in listBox_links.Items)
-            {
-                saveFile.WriteLine(item.ToString());
-            }
-            saveFile.Close();
+                if (listBox_links.Items.Count == 0)
+                {
+                    return;
+                }
+                StreamWriter saveFile = new StreamWriter("url.txt");
+                foreach (var item in listBox_links.Items)
+                {
+                    saveFile.WriteLine(item.ToString());
+                }
+                saveFile.Close();
 
-            if (!File.Exists("youtube-dl.exe") | !File.Exists("ffmpeg.exe") | !File.Exists("ffprobe.exe"))
-            {
-                MessageBox.Show("Önce youtube-dl ve ffmpeg güncellemelisiniz.","Uyarı",MessageBoxButtons.OK,MessageBoxIcon.Error);
-                return;
-            }
+                if (!File.Exists("youtube-dl.exe") | !File.Exists("ffmpeg.exe") | !File.Exists("ffprobe.exe"))
+                {
+                    MessageBox.Show("Önce youtube-dl ve ffmpeg güncellemelisiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
 
-            DownloadMP3();
+                youtubeService.DownloadMP3();
+            }
+            catch (Exception exception)
+            {
+
+                LoggingTool.log.Error("Hata oluştu.", exception);
+            }
+            
         }
 
         private void Main_Load(object sender, EventArgs e)
---
+++ YoutubeMP3.UI/YoutubeService.cs
@@ -0,0 +1,103 @@
+﻿using SlavaGu.ConsoleAppLauncher;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YoutubeMP3.Core;
+using YoutubeMP3.Core.Utilities;
+
+namespace YoutubeMP3.UI
+{
+    internal class YoutubeService
+    {
+        private RichTextBox richTextBox;
+        private ProgressBar progressBar;
+        private ListBox listBox;
+
+        public YoutubeService(RichTextBox richTextBox,ProgressBar progressBar,ListBox listBox)
+        {
+            this.richTextBox = richTextBox;
+            this.progressBar = progressBar;
+            this.listBox = listBox;
+        }
+
+        public async void DownloadComponents()
+        {
+            richTextBox.Text = "Downloading youtube-dl.exe and ffmpeg-release-essentials.7z";
+            LoggingTool.log.Info("Downloading youtube-dl.exe and ffmpeg-release-essentials.7z");
+            await Task.Run(() =>
+            {
+                if (File.Exists("youtube-dl.exe"))
+                {
+                    File.Delete("youtube-dl.exe");
+                }
+                if (File.Exists("ffmpeg.exe"))
+                {
+                    File.Delete("ffmpeg.exe");
+                }
+                if (File.Exists("ffprobe.exe"))
+                {
+                    File.Delete("ffprobe.exe");
+                }
+                if (File.Exists("ffmpeg-release-essentials.7z"))
+                {
+                    File.Delete("ffmpeg-release-essentials.7z");
+                }
+                FileDownloader fd = new FileDownloader(progressBar);
+                fd.Download("https://youtube-dl.org/downloads/latest/youtube-dl.exe", "youtube-dl.exe");
+                fd.Download("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.7z", "ffmpeg-release-essentials.7z");
+                //Extract(reply => BeginInvoke((MethodInvoker)delegate { listBox_links.Items.Add(reply); }));
+
+            });
+
+            await Task.Run(Extract);
+
+            richTextBox.Text = "Güncelleme başarıyla tamamlandı.";
+            LoggingTool.log.Info("Güncelleme başarıyla tamamlandı.");
+            progressBar.Value = 100;
+        }
+
+        public async void DownloadMP3()
+        {
+            await Task.Run(() =>
+            {
+                ConsoleApp app = new ConsoleApp("youtube-dl.exe", "--verbose --extract-audio --audio-format mp3 --batch-file url.txt");
+                app.ConsoleOutput += App_ConsoleOutput;
+                app.Run();
+                app.WaitForExit();
+                richTextBox.Text = "İndirme başarıyla tamamlandı.";
+                LoggingTool.log.Info("İndirme başarıyla tamamlandı.");
+                listBox.Items.Clear();
+            });
+
+        }
+        private void App_ConsoleOutput(object sender, ConsoleOutputEventArgs e)
+        {
+            richTextBox.Text = e.Line;
+            LoggingTool.log.Info(e.Line);
+        }
+
+        //private void Extract(Action<string> replyHandler)
+        //{
+        //    ConsoleApp app = new ConsoleApp("7z.exe", "e -r ffmpeg-release-essentials.7z ffmpeg.exe ffprobe.exe");
+        //    app.ConsoleOutput += App_ConsoleOutput;    
+        //    app.Run();
+        //    app.WaitForExit();
+        //}
+        public void Extract()
+        {
+            progressBar.Value = 70;
+            ConsoleApp app = new ConsoleApp("7z.exe", "e -r ffmpeg-release-essentials.7z ffmpeg.exe ffprobe.exe");
+            app.ConsoleOutput += App_ConsoleOutput;
+            app.Run();
+            app.WaitForExit();
+            if (File.Exists("ffmpeg-release-essentials.7z"))
+            {
+                File.Delete("ffmpeg-release-essentials.7z");
+            }
+        }
+    }
+}
